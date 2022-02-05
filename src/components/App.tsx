0d18309f678ef86ae11addb6a1eef96f7228c8d7
ahmednkhan24 <ahmednkhan24@gmail.com>
2022-02-05 16:15:06 -0600

    add bootstrap

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -1,10 +1,11 @@
 import React from 'react';
+import Container from 'react-bootstrap/Container';
 
 const App: React.FC = () => {
   return (
-    <div>
+    <Container>
       <h1>hello world!!!!</h1>
-    </div>
+    </Container>
   );
 };
 
