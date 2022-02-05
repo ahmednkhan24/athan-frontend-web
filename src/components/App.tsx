dd6905d1f6493d6a255704e15a2bcb8c890a070d
ahmednkhan24 <ahmednkhan24@gmail.com>
2022-02-05 17:52:10 -0600

    navbar

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -1,11 +1,13 @@
 import React from 'react';
 import { BrowserRouter, Switch, Route, Redirect } from 'react-router-dom';
+import NavBar from './NavBar';
 import Home from './Home';
 import Content from './Content';
 
 const App: React.FC = () => {
   return (
     <BrowserRouter>
+      <NavBar />
       <Switch>
         <Route exact path="/" component={Home} />
         <Route exact path="/content" component={Content} />
--- src/components/Content.tsx
+++ src/components/Content.tsx
@@ -1,11 +1,12 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import Container from 'react-bootstrap/Container';
 
 const Content: React.FC = () => (
-  <div>
-    <h1>content page</h1>
+  <Container>
+    <h1>Content page</h1>
     <Link to="/home">Click to go to Home</Link>
-  </div>
+  </Container>
 );
 
 export default Content;
