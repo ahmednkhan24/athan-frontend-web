53c2adc51689bd96dad971174b4d03b8045a53f9
ahmednkhan24 <ahmednkhan24@gmail.com>
2022-02-05 17:57:14 -0600

    restructure folders

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -1,20 +1,8 @@
 import React from 'react';
-import { BrowserRouter, Switch, Route, Redirect } from 'react-router-dom';
-import NavBar from './NavBar';
-import Home from './Home';
-import Content from './Content';
+import Router from './navigation';
 
 const App: React.FC = () => {
-  return (
-    <BrowserRouter>
-      <NavBar />
-      <Switch>
-        <Route exact path="/" component={Home} />
-        <Route exact path="/content" component={Content} />
-        <Route render={() => <Redirect to="/" />} />
-      </Switch>
-    </BrowserRouter>
-  );
+  return <Router />;
 };
 
 export default App;
