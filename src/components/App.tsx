06659643e78843606e977a0eb4365318612f024f
ahmednkhan24 <ahmednkhan24@gmail.com>
2022-02-05 18:29:30 -0600

    restructure folders

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -1,8 +1,20 @@
 import React from 'react';
-import Router from './navigation';
+import { Switch, Route, Redirect } from 'react-router-dom';
+import NavBar from './navbar';
+import Home from './screens/Home';
+import Content from './screens/Content';
 
 const App: React.FC = () => {
-  return <Router />;
+  return (
+    <>
+      <NavBar />
+      <Switch>
+        <Route exact path="/" component={Home} />
+        <Route exact path="/content" component={Content} />
+        <Route render={() => <Redirect to="/" />} />
+      </Switch>
+    </>
+  );
 };
 
 export default App;
--- src/components/navbar/index.tsx
+++ src/components/navbar/index.tsx
@@ -14,9 +14,6 @@ const NavBar: React.FC = () => {
         <Navbar.Toggle aria-controls="basic-navbar-nav" />
         <Navbar.Collapse id="basic-navbar-nav">
           <Nav className="me-auto">
-            <Nav.Link as={Link} to="/">
-              Home
-            </Nav.Link>
             <Nav.Link as={Link} to="/content">
               Content
             </Nav.Link>
