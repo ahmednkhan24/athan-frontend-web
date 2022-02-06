e542eef78799c2f0c5e110a7ff84fc3166eac3cd
ahmednkhan24 <ahmednkhan24@gmail.com>
2022-02-05 19:01:56 -0600

    capitalize

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -1,8 +1,8 @@
 import React from 'react';
 import { Switch, Route, Redirect } from 'react-router-dom';
-import NavBar from './navbar';
-import Home from './screens/Home';
-import Content from './screens/Content';
+import NavBar from './Navbar';
+import Home from './Screens/Home';
+import Content from './Screens/Content';
 
 const App: React.FC = () => {
   return (
