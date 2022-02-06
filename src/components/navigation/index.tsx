93038c08fcb7e931c471dce57cde437618b20e5a
ahmednkhan24 <ahmednkhan24@gmail.com>
2022-02-05 18:04:57 -0600

    use hash router

--- src/components/navigation/index.tsx
+++ src/components/navigation/index.tsx
@@ -1,19 +1,19 @@
 import React from 'react';
-import { BrowserRouter, Switch, Route, Redirect } from 'react-router-dom';
+import { HashRouter, Switch, Route, Redirect } from 'react-router-dom';
 import NavBar from './NavBar';
 import Home from '../screens/Home';
 import Content from '../screens/Content';
 
 const Router: React.FC = () => {
   return (
-    <BrowserRouter>
+    <HashRouter>
       <NavBar />
       <Switch>
         <Route exact path="/" component={Home} />
         <Route exact path="/content" component={Content} />
         <Route render={() => <Redirect to="/" />} />
       </Switch>
-    </BrowserRouter>
+    </HashRouter>
   );
 };
 
