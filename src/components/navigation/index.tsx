ae33a17568b61dc5e980ef2a0f06ad58c70c82a1
ahmednkhan24 <ahmednkhan24@gmail.com>
2022-02-05 18:20:46 -0600

    Revert "use hash router"
    This reverts commit 93038c08fcb7e931c471dce57cde437618b20e5a.

--- src/components/navigation/index.tsx
+++ src/components/navigation/index.tsx
@@ -1,19 +1,19 @@
 import React from 'react';
-import { HashRouter, Switch, Route, Redirect } from 'react-router-dom';
+import { BrowserRouter, Switch, Route, Redirect } from 'react-router-dom';
 import NavBar from './NavBar';
 import Home from '../screens/Home';
 import Content from '../screens/Content';
 
 const Router: React.FC = () => {
   return (
-    <HashRouter>
+    <BrowserRouter>
       <NavBar />
       <Switch>
         <Route exact path="/" component={Home} />
         <Route exact path="/content" component={Content} />
         <Route render={() => <Redirect to="/" />} />
       </Switch>
-    </HashRouter>
+    </BrowserRouter>
   );
 };
 
